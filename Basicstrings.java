b335a6bfb1e0e8817f51da98ada0ec27a14ee30e
bhavana <challabhavana786@gmail.com>
2026-01-28 22:08:29 +0530

    basic java programs

---
+++ Basicstrings.java
@@ -0,0 +1,16 @@
+
+class Human{
+     private int age;
+     private String name;
+     public String nam(){
+        name="bhavan";
+        return name;
+     }
+}
+public class Basicstrings {
+    public static void main(String[] args){
+        Human obj1 = new Human();
+        
+        System.out.println(obj1.nam()+":");
+    }
+}
